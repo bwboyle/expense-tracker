a1323e462cc95e9cc7c8cade572631786df7f7b6
Brendan Boyle <brendan.boyle02@gmail.com>
2022-01-27 01:23:07 -0600

    Sign removed

--- client/src/components/Transaction.js
+++ client/src/components/Transaction.js
@@ -5,11 +5,11 @@ import { numberWithCommas } from '../utils/format';
 export const Transaction = ({ transaction }) => {
   const { deleteTransaction } = useContext(GlobalContext);
 
-  const sign = transaction.amount < 0 ? '-' : '+'
+  // const sign = transaction.amount < 0 ? '-' : '+'
 
   return (
     <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
-        {transaction.text} <span>{sign}${numberWithCommas(Math.abs(transaction.amount))}</span>
+        {transaction.text} <span>${numberWithCommas(Math.abs(transaction.amount))}</span>
         <button className='delete-btn' onClick={() => deleteTransaction(transaction._id)}>x</button>
     </li>
   );
